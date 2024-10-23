93186aa18503561dda7193fc8a3d4db203f3aa76
peterblasko <blasko.peter.hu@gmail.com>
2024-10-23 14:29:17 +0200

    Special excel writer

--- src/main/java/hu/posta/szekesfehervar/service/ExcelWriterService.java
+++ src/main/java/hu/posta/szekesfehervar/service/ExcelWriterService.java
@@ -13,28 +13,45 @@ import org.springframework.stereotype.Service;
 public class ExcelWriterService {
 
     public static String filePath = "/Users/blaskopeter/Desktop/posta1.xlsx";
+    public static String [][] arrays = {
+            {"1", "3", "No"},
+            {"2", "3", "75"},
+            {"3", "3", ""},
+            {"4", "3", ""},
+            {"5", "3", "No"},
+            {"6", "3", "759"},
+            {"7", "3", "Yes"},
+            {"8", "3", "15"},
+            {"9", "3", "44"},
+            {"10", "3", "35"},
+            {"11", "3", ""},
+            {"12", "3", "187"},
+            {"1", "4", "No"},
+            {"2", "4", "75"},
+            {"13", "4", "187"},
+            {"14", "4", "Good"}
+    };
 
     public void excelWriter () {
         try (FileInputStream fis = new FileInputStream(filePath);
              XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+                Sheet sheet = workbook.getSheetAt(0);
 
-            Sheet sheet = workbook.getSheetAt(0);
+                for(String [] array : arrays) {
+                    Row row = sheet.getRow(Integer.parseInt(array[0]));
+                    if (row == null) {
+                        row = sheet.createRow(Integer.parseInt(array[0]));
+                    }
+                    Cell cell = row.getCell(Integer.parseInt(array[1]));
+                    if (cell == null) {
+                        cell = row.createCell(Integer.parseInt(array[1]));
+                    }
+                    cell.setCellValue(array[2]);
+                }
 
-            Row row = sheet.getRow(1);
-            if (row == null) {
-                row = sheet.createRow(1);
-            }
-            Cell cell = row.getCell(4);
-            if (cell == null) {
-                cell = row.createCell(4);
-            }
-            cell.setCellValue("Új érték");
-
-
-            try (FileOutputStream fos = new FileOutputStream("/Users/blaskopeter/Desktop/postaDemo2.xlsx")) {
+            try (FileOutputStream fos = new FileOutputStream("/Users/blaskopeter/Desktop/postaDemo3.xlsx")) {
                 workbook.write(fos);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
