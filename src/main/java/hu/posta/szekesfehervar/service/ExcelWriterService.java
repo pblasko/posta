317f5dcdf1ad9d28783619dee582dd9aaab6325c
peterblasko <blasko.peter.hu@gmail.com>
2024-10-23 15:42:03 +0200

    3D array - create more xls files

--- src/main/java/hu/posta/szekesfehervar/service/ExcelWriterService.java
+++ src/main/java/hu/posta/szekesfehervar/service/ExcelWriterService.java
@@ -13,7 +13,10 @@ import org.springframework.stereotype.Service;
 public class ExcelWriterService {
 
     public static String filePath = "/Users/blaskopeter/Desktop/posta1.xlsx";
-    public static String [][] arrays = {
+    public static String [] names = {"Bréda Attila", "Kővári László", "Vízvári Róbert"};
+    public static int index = 0;
+    public static String [][][] arrays = {
+        {
             {"1", "3", "No"},
             {"2", "3", "75"},
             {"3", "3", ""},
@@ -30,30 +33,67 @@ public class ExcelWriterService {
             {"2", "4", "75"},
             {"13", "4", "187"},
             {"14", "4", "Good"}
+        }, {
+            {"1", "3", "Yes"},
+            {"2", "3", "15"},
+            {"3", "3", "2"},
+            {"4", "3", "33"},
+            {"5", "3", ""},
+            {"6", "3", ""},
+            {"7", "3", ""},
+            {"8", "3", "15"},
+            {"9", "3", "14"},
+            {"10", "3", "45"},
+            {"11", "3", "4"},
+            {"12", "3", "187"},
+            {"1", "4", "No"},
+            {"2", "4", "23"},
+            {"13", "4", "187"},
+            {"14", "4", "Good"}
+        }, {
+            {"1", "3", "No"},
+            {"2", "3", "75"},
+            {"3", "3", ""},
+            {"4", "3", "33"},
+            {"5", "3", "No"},
+            {"6", "3", "759"},
+            {"7", "3", "Yes"},
+            {"8", "3", "15"},
+            {"9", "3", "44"},
+            {"10", "3", "35"},
+            {"11", "3", ""},
+            {"12", "3", "187"},
+            {"1", "4", "No"},
+            {"2", "4", "75"},
+            {"13", "4", "187"},
+            {"14", "4", "Good"}
+        }
     };
 
     public void excelWriter () {
-        try (FileInputStream fis = new FileInputStream(filePath);
-             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+        for(String [][] array : arrays) {
+            try (FileInputStream fis = new FileInputStream(filePath);
+                 XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
                 Sheet sheet = workbook.getSheetAt(0);
 
-                for(String [] array : arrays) {
-                    Row row = sheet.getRow(Integer.parseInt(array[0]));
+                for(String [] data : array) {
+                    Row row = sheet.getRow(Integer.parseInt(data[0]));
                     if (row == null) {
-                        row = sheet.createRow(Integer.parseInt(array[0]));
+                        row = sheet.createRow(Integer.parseInt(data[0]));
                     }
-                    Cell cell = row.getCell(Integer.parseInt(array[1]));
+                    Cell cell = row.getCell(Integer.parseInt(data[1]));
                     if (cell == null) {
-                        cell = row.createCell(Integer.parseInt(array[1]));
+                        cell = row.createCell(Integer.parseInt(data[1]));
                     }
-                    cell.setCellValue(array[2]);
+                    cell.setCellValue(data[2]);
                 }
-
-            try (FileOutputStream fos = new FileOutputStream("/Users/blaskopeter/Desktop/postaDemo3.xlsx")) {
-                workbook.write(fos);
+                try (FileOutputStream fos = new FileOutputStream("/Users/blaskopeter/Desktop/" + names[index] + ".xlsx")) {
+                    workbook.write(fos);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            index++;
         }
     }
 
